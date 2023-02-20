db91f8d3d7c3fca6cfbfa9221d80141741c8bd98
Jocemar Silva <jocemarsilva.dev@gmail.com>
2023-02-20 11:42:01 -0300

    Adiciona const res await fetch apiWeatherURL

--- script.js
+++ script.js
@@ -31,3 +31,4 @@ const getWeatherData = async (city) => {
 
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
+  const res = await fetch(apiWeatherURL);
