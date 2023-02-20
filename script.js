d600fdf70a89de74bc54a57530efc2854fb47722
Jocemar Silva <jocemarsilva.dev@gmail.com>
2023-02-20 11:27:51 -0300

    Adiciona const cityInput document.query

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const apiKey = "0880f46ec9c4ac0d5635cca36f17baab";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
