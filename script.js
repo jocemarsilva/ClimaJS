5dc413f95a400c1d8d132e93974d724dbd6dd231
Jocemar Silva <jocemarsilva.dev@gmail.com>
2023-02-20 11:31:34 -0300

    Adiciona const weatherIconElement

--- script.js
+++ script.js
@@ -8,4 +8,5 @@ const searchBtn = document.querySelector("#search");
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
 
